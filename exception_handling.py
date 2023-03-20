da9af9a7474144de362ce194a7761bef9b39a45e
Rakeshkhadka <rakesh23khadka@gmail.com>
2023-03-20 15:48:35 +0545

    excetion handling learning

--- exception_handling.py
+++ exception_handling.py
@@ -1,37 +1,37 @@
-# try:
-#     print(x)
-# except:
-#     print(" x is not initialized")
+try:
+    print(x)
+except:
+    print(" x is not initialized")
 
 
-# try:
-#     print(a/b)
-# except ZeroDivisionError:
-#     print("envountered 0 on divisor")
-# except:
-#     print("invalid")
+try:
+    print(a/b)
+except ZeroDivisionError:
+    print("envountered 0 on divisor")
+except:
+    print("invalid")
 
 
 
-# try:
-#     with open("data.csv") as file:
-#         file.read()
-# except FileNotFoundError as fnf:
-#     print(fnf)
-#     print("File Not found")
+try:
+    with open("data.csv") as file:
+        file.read()
+except FileNotFoundError as fnf:
+    print(fnf)
+    print("File Not found")
 
 
-# def find_nth(x, n):
-#     try:
-#         value = (x[n])
-#     except IndexError as IE:
-#         print(IE)
-#     else:
-#         print(value)
+def find_nth(x, n):
+    try:
+        value = (x[n])
+    except IndexError as IE:
+        print(IE)
+    else:
+        print(value)
 
 
-# y = [2, 3, 4, 5]
-# find_nth(y, 6)
+y = [2, 3, 4, 5]
+find_nth(y, 6)
 
 def divise(x, y):
     try:
