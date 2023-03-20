a006893d6b005a92c1787ba64a54af2dab37c669
Rakeshkhadka <rakesh23khadka@gmail.com>
2023-03-20 14:45:33 +0545

    typecasting examples

---
+++ tpecasting.py
@@ -0,0 +1,18 @@
+#explicit typecasting
+x = 23
+y = float(x)
+print(type(y))
+
+
+a = 45.78
+b = int(a)
+print(b)
+print(type(b))
+
+
+#implicit typecasting
+
+m = 4503
+n = 20.452
+print(m*n)
+print(type(m*n))
