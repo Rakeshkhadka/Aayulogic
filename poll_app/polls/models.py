b5d14d61d99fda731a5ce8e8cab1205fa61dc61b
Rakeshkhadka <rakesh23khadka@gmail.com>
2023-04-10 17:33:59 +0545

    Upto part 3

--- poll_app/polls/models.py
+++ poll_app/polls/models.py
@@ -1,13 +1,18 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
 
 class Questions(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("publication date")
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choices(models.Model):
     questions = models.ForeignKey(Questions, on_delete=models.CASCADE)
     choice = models.CharField(max_length=200)
     vote_count = models.IntegerField(default=0)
+
--- poll_app/polls/views.py
+++ poll_app/polls/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Questions
+from django.template import loader
 
 # Create your views here.
 def main(request):
-    return HttpResponse("this is home pages")
+    latest_question_list = Questions.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    # output = ", ".join([q.question_text for q in latest_question_list])
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Questions, pk=question_id)
+    context = {
+        "question": question
+    }
+    return render(request, "polls/detail.html", context)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
